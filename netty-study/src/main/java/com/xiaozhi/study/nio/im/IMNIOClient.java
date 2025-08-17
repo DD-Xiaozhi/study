0b79a9000533d7b7949d8375b13376bf92e7f474
DD <ddxu@chatlabs.cn>
2025-08-17 17:15:02 +0800

    feat(nio): 实现基于 NIO 的简易 IM 系统
    - 新增 IMNIOClient 类实现客户端逻辑
    - 新增 ImNIOServer 类实现服务器逻辑- 支持多客户端连接和消息广播
    - 优化了用户信息读取和发送流程
    - 增加了异常处理和会话管理功能

---
+++ netty-study/src/main/java/com/xiaozhi/study/nio/im/IMNIOClient.java
@@ -0,0 +1,79 @@
+package com.xiaozhi.study.nio.im;
+
+import lombok.SneakyThrows;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
+import java.util.Scanner;
+
+/**
+ *
+ * @author DD
+ */
+public class IMNIOClient {
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        SocketChannel sc = SocketChannel.open();
+        sc.connect(new InetSocketAddress(9999));
+        sc.configureBlocking(false);
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        Selector selector = Selector.open();
+        sc.register(selector, SelectionKey.OP_READ);
+        Scanner scanner = new Scanner(System.in);
+
+        // 处理服务器响应消息
+        Thread.startVirtualThread(() -> {
+            while (true) {
+                try {
+                    selector.select();
+                    Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+                    while (iterator.hasNext()) {
+                        SelectionKey selectionKey = iterator.next();
+                        iterator.remove();
+                        SocketChannel channel = (SocketChannel) selectionKey.channel();
+                        if (selectionKey.isAcceptable()) {
+                            channel.configureBlocking(false);
+                            channel.register(selector, SelectionKey.OP_READ);
+                        } else if (selectionKey.isReadable()) {
+                            int len;
+                            if ((len = sc.read(buffer)) != -1) {
+                                buffer.flip();
+                                String msg = new String(buffer.array(), 0, len, StandardCharsets.UTF_8);
+                                System.out.println(STR."\r\{msg}");
+                                System.out.print("请输入你要发送的消息：");
+                            }
+                        }
+                        buffer.clear();
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        // 处理用户信息读取发送服务
+        System.out.print("请输入你要发送的消息：");
+        while (true) {
+            String msg = scanner.nextLine();
+            if (StringUtils.isNotBlank(msg)) {
+                buffer.put(msg.getBytes(StandardCharsets.UTF_8));
+                try {
+                    buffer.flip();
+                    sc.write(buffer);
+                    System.out.print(STR."\r我：\{msg}\n");
+                    buffer.clear();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+}
---
+++ netty-study/src/main/java/com/xiaozhi/study/nio/im/ImNIOServer.java
@@ -0,0 +1,71 @@
+package com.xiaozhi.study.nio.im;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 基于 NIO 实现 IM
+ *
+ * @author DD
+ */
+public class ImNIOServer {
+
+    public static void main(String[] args) throws IOException {
+        Map<SocketChannel, String> sessionMap = new ConcurrentHashMap<>();
+        ServerSocketChannel ssc = ServerSocketChannel.open().bind(new InetSocketAddress(9999));
+        ssc.configureBlocking(false);
+        Selector selector = Selector.open();
+        ssc.register(selector, SelectionKey.OP_ACCEPT);
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        while (true) {
+            selector.select();
+            Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+            while (iterator.hasNext()) {
+                SelectionKey selectionKey = iterator.next();
+                try {
+                    iterator.remove();
+                    SocketChannel socketChannel = ssc.accept();
+                    if (selectionKey.isAcceptable()) {
+                        socketChannel.configureBlocking(false);
+                        socketChannel.register(selector, SelectionKey.OP_READ);
+                        // 将连接加入到会话中
+                        String sessionId = UUID.randomUUID().toString();
+                        sessionMap.put(socketChannel, sessionId);
+                    } else if (selectionKey.isReadable()) {
+                        SocketChannel channel = (SocketChannel) selectionKey.channel();
+                        int len;
+                        if ((len = channel.read(byteBuffer)) != -1) {
+                            byteBuffer.flip();
+                            String msg = new String(byteBuffer.array(), 0, len);
+                            for (SocketChannel sessionChannel : sessionMap.keySet()) {
+                                if (sessionChannel != channel) {
+                                    String consent = STR."\{sessionMap.get(sessionChannel)}: \{msg}";
+                                    byteBuffer.clear();
+                                    byteBuffer.put(consent.getBytes(StandardCharsets.UTF_8));
+                                    byteBuffer.flip();
+                                    sessionChannel.write(byteBuffer);
+                                }
+                            }
+                        }
+                        byteBuffer.clear();
+                    }
+                } catch (IOException e) {
+                    SocketChannel channel = (SocketChannel) selectionKey.channel();
+                    System.out.println(STR."客户端连接异常或重置: [ ip: \{channel.getRemoteAddress()}, error msg: \{e.getMessage()} ]}");
+
+                    // 关闭通道和监听
+                    channel.close();
+                    selectionKey.channel();
+                    sessionMap.remove(channel);
+                }
+            }
+        }
+    }
+}
